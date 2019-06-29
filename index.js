8340444e811b03eb55e941329283f00136cb89e0
kalvin <kalvinvini1998@gmail.com>
2019-06-29 12:41:23 -0300

    rename contract file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-const BlockmusicContract = require('./lib/album-contract');
+const BlockmusicContract = require('./lib/blockmusic-contract');
 
 module.exports.BlockmusicContract = BlockmusicContract;
 module.exports.contracts = [ BlockmusicContract ];
